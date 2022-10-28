527d63d83560fafb09d9edb2eefc94ad7783b905
김영민 <rpan0522@gmail.com>
2022-10-29 00:13:45 +0900

    실습일지

---
+++ 9-1-1/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PassingCount from './chapter11/PassingCount';
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+        <PassingCount/>
+);
