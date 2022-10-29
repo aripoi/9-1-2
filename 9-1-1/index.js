e2cc1dd6c4382e43359e9fd949c2342bc35baa86
aripoi <106456245+aripoi@users.noreply.github.com>
2022-10-30 03:24:18 +0900

    Delete 9-1-1 directory

--- 9-1-1/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PassingCount from './chapter11/PassingCount';
-
-
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-        <PassingCount/>
-);
